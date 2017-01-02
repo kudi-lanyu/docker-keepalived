57113d783aa1027bb0bbb909b7a61ed356f4e96b
Dawid Malinowski <dawidmalina@gmail.com>
2017-01-02 22:44:02 +0100

    Remove unused variables

--- start-keepalived.sh
+++ start-keepalived.sh
@@ -4,18 +4,12 @@ set -e
 echo "=> Configuring Keepalived"
 
 sed -i -e "s/<--VROUTERID-->/${VROUTERID}/g" /etc/keepalived/keepalived.conf
-
 sed -i -e "s/<--VIP-->/${VIP}/g" /etc/keepalived/keepalived.conf
-sed -i -e "s/<--MASK-->/${MASK}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--STATE-->/${STATE}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--INTERFACE-->/${INTERFACE}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--PRIORITY-->/${PRIORITY}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--ADVERT-->/${ADVERT}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--AUTHPASS-->/${AUTHPASS}/g" /etc/keepalived/keepalived.conf
-
-sed -i -e "s/<--NOTIFIEMAILTO-->/${NOTIFIEMAILTO}/g" /etc/keepalived/keepalived.conf
-sed -i -e "s/<--NOTIFIEMAILFROM-->/${NOTIFIEMAILFROM}/g" /etc/keepalived/keepalived.conf
-sed -i -e "s/<--SMTPSERV-->/${SMTPSERV}/g" /etc/keepalived/keepalived.conf
 
 echo "=> Starting Keepalived ... : "
 
