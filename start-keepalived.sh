e02b404990983deb2d8d4c3ea029c154536fbe86
lukasbar <l.baranski@oberthur.com>
2017-01-11 14:28:45 +0100

    syntax

--- start-keepalived.sh
+++ start-keepalived.sh
@@ -9,11 +9,11 @@ sed -i -e "s/<--INTERFACE-->/${INTERFACE}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--PRIORITY-->/${PRIORITY:-100}/g" /etc/keepalived/keepalived.conf
 sed -i -e "s/<--AUTHPASS-->/${AUTHPASS}/g" /etc/keepalived/keepalived.conf
 
-for VIP in $( env |grep VIP |sort | awk -F "=" '{print $2}' )
+for VIP in $( env | grep VIP | sort | awk -F "=" '{print $2}' )
 do
-  VIPx=$( env |grep ${VIP} |sed 's/^[^=]*=//' )
+  VIPx=$( env | grep ${VIP} | sed 's/^[^=]*=//' )
   echo ${VIPx} > ${VIP}
-  if [ -n "$( env |grep VIP )" ]; then
+  if [ -n "$( env | grep VIP )" ]; then
   sed -i -e "/virtual_ipaddress {/a\        ${VIPx}" /etc/keepalived/keepalived.conf;
 fi
 echo "VIP's added"
